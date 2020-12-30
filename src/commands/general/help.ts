9800a7baa830675fe07d17f0f3ea26772257164f
Andrew Nijmeh <andrewnijmeh1@gmail.com>
2020-12-30 10:46:50 -0500

    define class name for help

--- src/commands/general/help.ts
+++ src/commands/general/help.ts
@@ -2,7 +2,7 @@ import Command from "../../lib/structures/Command";
 import { KcalsMessage } from "../../lib/types/kcalsbot";
 import { MODE } from "../../lib/utils/constants";
 
-export default class extends Command {
+export default class Help extends Command {
   aliases = ["info", "support"];
   dm = true;
   mode = MODE.STRICT;
