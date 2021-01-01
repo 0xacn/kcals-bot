4820d713f725e707c4ddce25747f4f66d8ebbe4b
Simon Risberg <simon@risberg.eu>
2021-01-01 03:31:35 +0100

    feat: i18n and ping command

--- src/lib/KcalsClient.ts
+++ src/lib/KcalsClient.ts
@@ -3,9 +3,11 @@ import "./extensions/KcalsGuildMember";
 import "./extensions/KcalsMessage";
 
 import { Client } from "discord.js";
-
+import { TFunction } from "i18next";
 import { Client as VezaClient } from "veza";
 import Logger from "./utils/Logger";
+import i18n from "./utils/i18n";
+import pkg from "../../package.json";
 
 // Handlers
 import CommandHandler from "../handlers/CommandHandler";
@@ -21,7 +23,9 @@ export default class Cluster extends Client {
   public commands = new CommandHandler(this);
   public events = new EventHandler(this);
 
+  public translate: Map<string, TFunction> = new Map();
   public logger = new Logger();
+  public version = pkg.version;
   public owners: string[] = ["Simonn#6377"];
 
   public constructor(node: VezaClient | undefined) {
@@ -46,6 +50,10 @@ export default class Cluster extends Client {
 
     // Setup handlers
     // ...
+
+    // Setup translation i18n before login to client
+    this.translate = await i18n();
+    this.logger.info("Loaded i18n Languages");
 
     return super.login(token);
   }
